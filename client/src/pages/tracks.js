8696b159506cf06e181708d2a99d509105b68886
Nisha <nishashukla1987@gmail.com>
2021-02-10 23:36:26 +0100

    added exercise

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,37 @@
+import { useQuery, gql } from '@apollo/client';
 import React from 'react';
 import { Layout } from '../components';
+import TrackCard from '../containers/track-card';
 
-/**
- * Tacks Page is Catstronauts home page.
- * We display a grid of tracks fetched with useQuery with the TRACKS query
- */
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return 'Loading...';
+
+  if (error) return `Error! ${error.message}`;
+  return (
+    <Layout grid>
+      {data?.tracksForHome?.map((track) => (
+        <TrackCard key={track.id} track={track} />
+      ))}
+    </Layout>
+  );
 };
 
 export default Tracks;
